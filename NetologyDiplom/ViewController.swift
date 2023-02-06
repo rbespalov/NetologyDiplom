10030e3c54aca1d9ddd57cb8dd81f6e998d85d0a
Roman Bespalov <voordex@gmail.com>
2023-02-06 17:14:41 +0300

    Initial Commit

---
+++ NetologyDiplom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetologyDiplom
+//
+//  Created by Роман Беспалов on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
